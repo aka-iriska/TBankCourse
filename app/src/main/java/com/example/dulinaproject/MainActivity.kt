e5cb766ea0a075d2136e7fb1bf1d2ebf876850fd
Korina <trotil617@gmail.com>
2024-10-23 17:48:47 +0300

    исправила название RecyclerView и поле adapter

--- app/src/main/java/com/example/dulinaproject/MainActivity.kt
+++ app/src/main/java/com/example/dulinaproject/MainActivity.kt
@@ -3,59 +3,14 @@ package com.example.dulinaproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.dulinaproject.data.Joke
 import com.example.dulinaproject.databinding.ActivityMainBinding
-import com.example.dulinaproject.recycler.adapter.JokeListAdapter
-import com.example.dulinaproject.recycler.util.JokeItemDiffCallback
+import com.example.dulinaproject.recycler.JokeAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val adapter by lazy { JokeListAdapter(JokeItemDiffCallback()) }
-
-    private val data = listOf(
-        Joke(
-            category = "Программирование",
-            question = "Почему программисты не любят природу?",
-            answer = "Слишком много багов в лесу!"
-        ),
-        Joke(
-            category = "Программирование",
-            question = "Почему программистам сложно находить пару?",
-            answer = "Потому что они постоянно ищут переменную, а не постоянную!"
-        ),
-        Joke(
-            category = "Программирование",
-            question = "Какой любимый напиток у кодеров?",
-            answer = "Смузи!"
-        ),
-        Joke(
-            category = "Общество",
-            question = "Почему никто не берет в долг у математиков?",
-            answer = "Потому что они всегда оставляют проценты!"
-        ),
-        Joke(
-            category = "Общество",
-            question = "Как называют человека, который продал свою печень?",
-            answer = "Обеспеченный"
-        ),
-        Joke(
-            "Мультфильмы",
-            "Почему черепашки ниндзя нападают вчетвером?",
-            "У них учитель крыса"
-        ),
-        Joke(
-            "Природа",
-            "Что будет, если ворона сядет на оголённые провода?",
-            "Электро Кар"
-        ),
-        Joke(
-            "Общество",
-            "Где лежат книги о дискриминации карликов?",
-            "На верхней полке"
-        )
-    )
+    private val adapter by lazy { JokeAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,7 +21,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        adapter.submitList(data)
         binding.jokesRecyclerView.adapter = adapter
         binding.jokesRecyclerView.layoutManager = LinearLayoutManager(this)
     }
