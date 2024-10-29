5c773bdae0754e41b9782085e74c40f7454ae1d6
Korina <trotil617@gmail.com>
2024-10-29 23:54:17 +0300

    added const value for `DEFAULT_JOKE_POSITION` added MVVM fixed main issues screens Added new screen * detailed view of every joke

---
+++ app/src/main/java/com/example/dulinaproject/ui/jokeList/recycler/adapter/JokeListAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.dulinaproject.ui.jokeList.recycler.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dulinaproject.data.Joke
+import com.example.dulinaproject.databinding.JokeItemBinding
+import com.example.dulinaproject.ui.jokeList.recycler.JokeViewHolder
+import com.example.dulinaproject.ui.jokeList.recycler.util.JokeItemDiffCallback
+
+class JokeListAdapter(
+    itemCallback: JokeItemDiffCallback,
+    private val clickListener: (Int) -> Unit
+) : ListAdapter<Joke, JokeViewHolder>(itemCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+
+        Log.d("viewType", viewType.toString())
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = JokeItemBinding.inflate(inflater, parent, false)
+
+        return JokeViewHolder(binding).apply {
+            binding.root.setOnClickListener {
+                handleJokeClick(adapterPosition)
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int = currentList.size
+
+    // вызывается при скроллинге
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    private fun handleJokeClick(position: Int) {
+        if (position != RecyclerView.NO_POSITION) {
+            currentList[position]?.let {
+                Log.d("giving position:", position.toString())
+                clickListener(position)
+            }
+        }
+    }
+}
