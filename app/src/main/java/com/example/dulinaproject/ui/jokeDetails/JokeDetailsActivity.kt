b04afdaaaf3514a5f272aaf27657f132bb2f111c
Korina <trotil617@gmail.com>
2024-11-09 14:33:49 +0300

    fixed main issues

--- app/src/main/java/com/example/dulinaproject/ui/jokeDetails/JokeDetailsActivity.kt
+++ app/src/main/java/com/example/dulinaproject/ui/jokeDetails/JokeDetailsActivity.kt
@@ -3,7 +3,6 @@ package com.example.dulinaproject.ui.jokeDetails
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dulinaproject.data.Joke
@@ -14,19 +13,18 @@ class JokeDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityJokeDetailsBinding
 
-    val data = JokeData.data
-
-    private var jokePosition: Int = -1
+    private val data by lazy {JokeData.data}
 
     // правило открытия activity
     companion object {
+
+        private const val JOKE_POSITION_EXTRA_DEFAULT_VALUE: Int = -1
 
         private const val JOKE_POSITION_EXTRA = "JOKE_POSITION"
 
         fun getInstance(context: Context, jokePosition: Int): Intent {
             return Intent(context, JokeDetailsActivity::class.java).apply {
                 putExtra(JOKE_POSITION_EXTRA, jokePosition)
-                Log.d("position", jokePosition.toString())
             }
         }
     }
@@ -40,7 +38,7 @@ class JokeDetailsActivity : AppCompatActivity() {
     }
 
     private fun handleExtra() {
-        jokePosition = intent.getIntExtra(JOKE_POSITION_EXTRA, -1)
+        val jokePosition = intent.getIntExtra(JOKE_POSITION_EXTRA, JOKE_POSITION_EXTRA_DEFAULT_VALUE)
 
         if (jokePosition == -1) {
             handleError()
